a800b3c3cb8fff3d2b02476a2f9df1d194efff1a
ameyaraje <ameyacraje@gmail.com>
2018-08-21 23:02:23 -0500

    add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 const app = express();
 
+// Map global promise
+mongoose.Promise = global.Promise;
+// Connect to Mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+	useMongoClient: true
+})
+.then(() => console.log('MongoDB Connected!'))
+.catch(err => console.log('Error connecting to MongoDB'));
+
+// Load IdeaModel
+
+require('./models/idea');
+const Idea = mongoose.model('ideas');
 // Middleware that can be used
 
 // app.use(function(req, res, next){
@@ -27,6 +41,11 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
 	res.render('about');
+});
+
+// Add Idea Form
+app.get('/ideas/add', (req, res) => {
+	res.render('/ideas/add');
 });
 
 const port = 5000;
