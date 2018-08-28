6ccc4cc78e4b6f1d4e5c39ab4cbccc5a4aa0b785
ameyaraje <ameyacraje@gmail.com>
2018-08-27 23:09:46 -0500

    added all CRUD operations

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session')
 
 const app = express();
 
@@ -24,12 +26,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 
+// Express session middleware
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true,
+}));
+
+app.use(flash());
 
 app.engine('handlebars', exphbs({
 	defaultLayout: 'main'
 }));
 
 app.set('view engine', 'handlebars');
+
+// Global variables
+app.use(function(req, res, next) {
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
 
 app.get('/', (req, res) => {
 	const name = 'Ameya';
@@ -63,7 +81,7 @@ app.get('/ideas/edit/:id', (req, res) => {
 			res.render('ideas/edit', {
 				idea: idea
 			})
-		} );
+		});
 });
 
 // Add Idea Form
@@ -92,6 +110,7 @@ app.post('/ideas', (req, res) => {
 			details: req.body.details,
 		};
 		new Idea(newUser).save().then(idea => {
+			req.flash('success_msg', 'Idea Added Successfully!');
 			res.redirect('/ideas');
 		});
 	}
@@ -102,20 +121,25 @@ app.put('/ideas/:id', (req, res) => {
 	Idea.findOne({
 		_id: req.params.id
 	})
-	.then(idea => {
-		idea.title = req.body.title;
-		idea.details = req.body.details;
-
-		idea.save()
 		.then(idea => {
-			res.redirect('/ideas');
+			idea.title = req.body.title;
+			idea.details = req.body.details;
+
+			idea.save()
+				.then(idea => {
+					req.flash('success_msg', 'Idea Updated Successfully!');
+					res.redirect('/ideas');
+				});
 		});
-	});
 });
 
 // Delete idea
 app.delete('/ideas/:id', (req, res) => {
-	
+	Idea.remove({ _id: req.params.id })
+		.then(() => {
+			req.flash('success_msg', 'Idea Removed Successfully!');
+			res.redirect('/ideas');
+		});
 });
 
 const port = 5000;
