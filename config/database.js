087882a518737a2a9bfbde2c8cb3267384400ad3
ameyaraje <ameyacraje@gmail.com>
2018-09-05 18:56:53 -0500

    corrected mongo user

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = {mongoURI: 'mongodb://<Ameya>:<vidjot1234>@ds119151.mlab.com:19151/vidjotdb-prod'};
+    module.exports = {mongoURI: 'mongodb://ameyaraje:ameya123@ds119151.mlab.com:19151/vidjotdb-prod'};
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'};
 }
